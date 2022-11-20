423abd65c8d865ebc3e01bf8d6b8344d30b3e5a7
Prasad <prasadhiwarkar18@gmail.com>
2022-11-20 12:35:01 +0530

    Knowing more about templates {% csrf_token %}

--- base/views.py
+++ base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python !'},
+    {'id': 2, 'name': 'Lets learn Java !'},
+    {'id': 3, 'name': 'Lets learn Cloud !'},
+    {'id': 4, 'name': 'Lets learn C++ !'}
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
 
 def room(request):
