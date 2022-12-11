6e123a2f7eeaa0c27950660ab8b07dcba7acd75a
Prasad <prasadhiwarkar18@gmail.com>
2022-12-11 19:45:41 +0530

    Messages in room.

--- base/views.py
+++ base/views.py
@@ -85,8 +85,17 @@ def home(request):
 
 def room(request, pk):
     room = models.Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
 
-    context = {'room': room}
+    if request.method == 'POST':
+        message = models.Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages}
 
     return render(request, 'base/room.html', context)
 
