c369e8aa8268cde3075bc5567880cef7d8c99395
Prasad <prasadhiwarkar18@gmail.com>
2022-12-13 23:56:42 +0530

    User Profile.

--- base/urls.py
+++ base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("logout/", views.logoutUser, name='logout'),
     path("", views.home, name='home'),
     path("room/<str:pk>/", views.room, name='room'),
+    path('profile/<str:pk>', views.userProfile, name='user-profile'),
     path('createRoom/', views.createRoom, name='create-room'),
     path('updateRoom/<str:pk>/', views.updateRoom, name='update-room'),
     path('deleteRoom/<str:pk>/', views.deleteRoom, name='delete-room'),
