b7f6ffeb0bf29f778d790880bc620e19618e3f15
Prasad <prasadhiwarkar18@gmail.com>
2022-12-14 10:53:47 +0530

    Static Files.

--- base/views.py
+++ base/views.py
@@ -112,7 +112,7 @@ def userProfile(request, pk):
     rooms = user.room_set.all()
     room_messages = user.message_set.all()
     topics = models.Topic.objects.all()
-    context = {'user': user, 'rooms': rooms, 'room-messages': room_messages, 'topics': topics}
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
     return render(request, 'base/profile.html', context)
 
 
@@ -123,7 +123,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context = {'form': form}
